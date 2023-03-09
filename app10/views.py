3984825e169d78f0f2cf0b294a6a6a07e7af69a9
basharb <you@example.com>
2023-03-09 16:31:26 +0530

    first commit

---
+++ app10/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def tata(request):
+    return HttpResponse("<h1>basha is a tcser</h1>")
+def rathan(request):
+    return HttpResponse("<h1>briged bhuwalka")
